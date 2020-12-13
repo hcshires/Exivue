6be79e6de4351495c46c94ddd0f4e43e34bb6477
Henry Shires <hcsgeek@gmail.com>
2020-12-13 15:27:38 -0600

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-manifest",
-    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
