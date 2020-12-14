52bd89a590bc91d7a6e0faf3b4ae261c7e26ca82
Henry Shires <hcsgeek@gmail.com>
2020-12-13 19:48:27 -0600

    Landing view

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,38 @@
 module.exports = {
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+	siteMetadata: {
+		title: "ExiVue 1.0",
+	},
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+		{
+			resolve: '@el7cosmos/gatsby-plugin-prefetch-google-fonts',
+			options: {
+				fonts: [
+					{
+						family: 'Exo 2',
+						variants: ['100', '400'],
+					},
+				],
+			},
+		},
+	],
 };
